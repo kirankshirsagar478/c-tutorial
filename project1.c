f46c9616e032c7c2c2d972598bd06803fffeede6
kirankshirsagar478 <kirankshirsagar478@gmail.com>
2021-12-13 18:54:54 +0530

    formated project 1

--- project1.c
+++ project1.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-    int number, guess, nguesses=1;
-    srand (time(0));
-    number= rand () % 100+1;
-    // Generate a random numberbetween 1 to 100 
+int main()
+{
+    int number, guess, nguesses = 1;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    // Generate a random numberbetween 1 to 100
     // printf ("The number is %d \n", number);
     // Keep running the loop until the number is guessed
-    do {
-        printf ("Guess the number between 1 to 100\n");
-        scanf ("%d",&guess);
-        if (guess>number) {
-            printf ("Lower Number Please!\n");
+    do
+    {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+        if (guess > number)
+        {
+            printf("Lower Number Please!\n");
         }
-        else if (guess<number) {
-            printf ("Higher Number Please\n");
+        else if (guess < number)
+        {
+            printf("Higher Number Please\n");
         }
-        else {
-            printf ("You guess it in %d attempts\n",nguesses);
+        else
+        {
+            printf("You guess it in %d attempts\n", nguesses);
         }
         nguesses++;
-    } while (guess!=number);
+    } while (guess != number);
     return 0;
 }
